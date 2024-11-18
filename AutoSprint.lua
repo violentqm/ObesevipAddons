4480396bad5c827974143f28f19f1b1e08d66a2f
Violentqm <79298794+violentqm@users.noreply.github.com>
2024-11-18 05:18:36 -0800

    Update AutoSprint.lua

--- AutoSprint.lua
+++ AutoSprint.lua
@@ -1,22 +1,28 @@
-local AlwaysSprint = ObeseAddons:Section({Name = "Always Sprint", Side = "left"})
-
-local RunService = game:GetService("RunService")
+local AlwaysSprint = ObeseAddons:Section({Name = "Auto Sprint", Side = "left"})
+local UIS = game:GetService("UserInputService")
+local isMoving = false
 local connection
 
+local function checkMovement()
+   return UIS:IsKeyDown(Enum.KeyCode.W) or UIS:IsKeyDown(Enum.KeyCode.A) or 
+          UIS:IsKeyDown(Enum.KeyCode.S) or UIS:IsKeyDown(Enum.KeyCode.D)
+end
+
 AlwaysSprint:Toggle({
-    Name = "Always Hold Shift", 
-    Flag = "AlwaysHoldShiftToggle", 
-    Def = false, 
-    Callback = function(enabled)
-        if enabled then
-            connection = RunService.Heartbeat:Connect(function()
-                keypress(0xA0)
-            end)
-        else
-            if connection then
-                connection:Disconnect()
-                keyrelease(0xA0)
-            end
-        end
-    end
+   Name = "Auto Sprint", 
+   Flag = "AutoSprintToggle", 
+   Def = false, 
+   Callback = function(enabled)
+       if enabled then
+           connection = game:GetService("RunService").RenderStepped:Connect(function()
+               local moving = checkMovement()
+               if moving ~= isMoving then
+                   isMoving = moving
+                   if moving then keypress(0xA0) else keyrelease(0xA0) end
+               end
+           end)
+       else
+           if connection then connection:Disconnect(); keyrelease(0xA0) end
+       end
+   end
 })
